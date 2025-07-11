c1b14fdc11cc952f89001764ae846a5a0f356300
Glória Maria <152623755+gloriadeitos@users.noreply.github.com>
2025-07-11 13:27:01 -0300

    Create q1.py

---
+++ final/q1.py
@@ -0,0 +1,15 @@
+'''
+1) Foram realizados voos fotogrametricos com diferentes alturas de voo, e mediu-se o tamanho do pixel (GSD) resultante nas imagens geradas.
+O objetivo é avaliar se existe correlação entre altura de voo e o GSD. Os dados coletados são:
+
+Ponto 1 → Altura=80, GSD(cm/pixel)=2,0
+Ponto 2 → Altura=100, GSD(cm/pixel)=2,5
+Ponto 3 → Altura=120, GSD(cm/pixel)=3,0
+Ponto 4 → Altura=140, GSD(cm/pixel)=3,5
+Ponto 5 → Altura=160, GSD(cm/pixel)=4,0
+
+a) Calcule o coeficiente de correlação entre altura de voo e o GSD.
+
+b) Analise o resultado baseado no gráfico: há correlação positiva, negativa ou nenhuma correlação? O resultado é coerente com o comportamento
+esperado?
+'''
