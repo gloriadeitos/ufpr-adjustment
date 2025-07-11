2a7a2c37b996d5a5bbe2c1a1ea89ea6198d0a505
Glória Maria <152623755+gloriadeitos@users.noreply.github.com>
2025-07-11 13:36:29 -0300

    Create q2.py

---
+++ final/q2.py
@@ -0,0 +1,12 @@
+'''
+2) Um engenheiro cartógrafo, durante uma campanha de levantamento geodésico, mediu cinco vezes o ângulo entre dois marcos utilizando uma estação total de alta precisão, em condições normais de operação.
+Os valores obtidos nas medições foram registrados na tabela a seguir:
+
+Observação 1 → 75° 14' 32,4"
+Observação 2 → 75° 14' 33,1"
+Observação 3 → 75° 14' 32,9"
+Observação 4 → 75° 14' 33,5"
+Observação 5 → 75° 14' 31,8"
+
+Estime o intervalo de confiança de 99% para média e variância.
+'''
