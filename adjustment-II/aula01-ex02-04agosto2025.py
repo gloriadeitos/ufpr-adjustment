4729bc0cd4faf31001b8515d692fa18f3e22a7bd
Glória Maria <152623755+gloriadeitos@users.noreply.github.com>
2025-08-04 15:19:23 -0300

    Create aula01-ex02-04agosto2025.py

---
+++ adjustment-II/aula01-ex02-04agosto2025.py
@@ -0,0 +1,7 @@
+"""
+O conjunto de dados, mostrados abaixo, representa a porção em segundos de arco de 50 medidas de uma direção.
+
+Calcular a amplitude, média média, e mediana, a moda, desvio padrão da amostra, desvio padrão da média, a variância e construir um histrograma de classes.
+"""
+
+# Dados fornecidos
