a64eb72b7fb8617848c54834c5bd0f5b737098c4
Glória Maria <152623755+gloriadeitos@users.noreply.github.com>
2025-08-11 14:55:27 -0300

    Update aula03-ex02-12agosto2025.py

--- adjustment-II/aula03-ex02-12agosto2025.py
+++ adjustment-II/aula03-ex02-12agosto2025.py
@@ -57,6 +57,10 @@ L = np.array([[3.1],
       [3.1]
     ])
 
+# a) Identificar se os sistemas de equações são consistentes e determinados.
+print("Como se pode calcular a matriz inversa e o determinante é zero, pode-se afirmar que o sistema é CONSISTENTE E INDETERMINADO")
+
+# b) Encontrar a solução que melhor representa o sistema, utilizando MMQ
 # RESPOSTA, Matriz das incógnitas
 """
 X = (A_transposta * A)^-1 * (A_transposta * L)
@@ -68,3 +72,103 @@ print("X =")
 for valor in X:
     print(f"[{valor[0]:.4f}]")
 print("\n Ou seja: \n X = \n [x] \n [y] \n [z] \n [w]")
+
+
+# ====================================================================
+
+"""
+MÉTODO DOS MÍNIMOS QUADRADOS
+
+Sistema de equações:
+- Consistente: quando existe PELO MENOS UMA solução que satisfaz todas as equações do modelo
+- Incosistente: quando NÃO POSSUI NENHUMA solução
+- Determinado: quando existe APENAS UMA solução que satisfaz todas as equações do modelo
+- Indeterminado: quando existem INFINITAS soluções que satisfazem todas as equações do modelo
+
+Dado o sistema de equações de observação:
+
+{ x + y = 3.1
+{ x - y = 0.9
+{ 2z + w = 4.9
+{ z - w = 4.1
+{ x + z - w = 3.9
+{ x - y + z - w = 3.1
+*é um sistema, então tudo é no mesmo {
+
+{ x + y = 3.1
+{ x + z = 3.9
+{ 2z = 4.9
+{ x - y + z = 3.1
+{ x - y = 0.9
+{ z = 4.1
+*é um sistema, então tudo é no mesmo {
+
+{ a + b + c + d = 15.25
+{ -a -b -c = 7.35
+{ 3a - 4b + 8c - d = 8.75
+{ b - c - d = 3.17
+*é um sistema, então tudo é no mesmo {
+
+a) Identificar se os sistemas de equações são consistentes e determinados.
+
+b) Se os sistemas são identificados por consistentes e determinados, encontrar a solução que
+melhor representa o sistema utilizando o princípio de mínimos quadrados.
+"""
+
+import numpy as np
+
+# Matriz do sistema de equações
+A = np.array([[1, 1, 0, 0],
+      [1, -1, 0, 0],
+      [0, 0, 2, 1],
+      [0, 0, 1, -1],
+      [1, 0, 1, -1],
+      [1, -1, 1, -1]
+    ])
+
+# Matriz das observações
+L = np.array([[3.1],
+      [0.9],
+      [4.9],
+      [4.1],
+      [3.9],
+      [3.1]
+    ])
+
+# a) Identificar se os sistemas de equações são consistentes e determinados.
+"""
+det(A_transposta * A) = [0.000]
+
+Se o determinante for:
+detA = 0 → INDETERMINADO
+detA ≠ 0 → DETERMINADO
+"""
+
+# Para calcular det(Aᵀ * A), primeiro calcula a matriz transposta e o produto
+ATA = A.T @ A
+
+det = np.linalg.det(ATA)
+
+# Vamos definir um limite para considerar o determinante como zero, devido à precisão numérica
+limiar = 1e-10
+
+if abs(det) < limiar:
+    print("Determinante é zero → SISTEMA INDETERMINADO")
+else:
+    print("Determinante diferente de zero → SISTEMA DETERMINADO")
+
+print("Como se pode calcular a matriz inversa e o determinante é zero, pode-se afirmar que o sistema é CONSISTENTE E INDETERMINADO")
+
+# b) Encontrar a solução que melhor representa o sistema, utilizando MMQ
+# RESPOSTA, Matriz das incógnitas
+"""
+X = (A_transposta * A)^-1 * (A_transposta * L)
+"""
+X = np.linalg.inv(A.T @ A) @ A.T @ L
+
+# Resposta com 4 casas decimais
+print("X =")
+for valor in X:
+    print(f"[{valor[0]:.4f}]")
+print("\n Ou seja: \n X = \n [x] \n [y] \n [z] \n [w]")
+
