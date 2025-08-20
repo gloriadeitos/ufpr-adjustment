47ac38a169163a3bdf15e6dc75dc85c9c97ddcbd
Glória Maria <152623755+gloriadeitos@users.noreply.github.com>
2025-08-20 13:58:05 -0300

    Update aula05-ex02-18agosto2025.py

--- adjustment-II/aula05-ex02-18agosto2025.py
+++ adjustment-II/aula05-ex02-18agosto2025.py
@@ -1,3 +1,16 @@
-""" 
-Fazer depois
+"""
+Uma linha de nivelamento foi feita ligando dois pontos A e D de altitudes conhecidas
+H_A=785.53m e H_D=842.00m, conforme figura abaixo. O sentido da seta indica a direção
+da estação mais elevada. Os pesos das observações são inversamente proporcionais aos
+comprimentos das linhas. As observações não são correlacionadas.
+
+Calcular as altitudes dos pontos B e C ajustadas usando ajustamento paramétrico
+
+  Seção | Desnível (m) | Distância (km)
+AB = h₁ |    32,54     |    2,0
+BC = h₂ |    5,93      |    1,0
+CD = h₃ |    17,97     |    2,5
+
+
+FAZER DEPOIS
 """
