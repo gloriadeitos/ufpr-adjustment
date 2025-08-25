c275ecb92864f8f9ea468a388981c4a9338d64b4
Glória Maria <152623755+gloriadeitos@users.noreply.github.com>
2025-08-25 15:21:26 -0300

    Create aula07-ex02-25agosto2025.py

---
+++ adjustment-II/aula07-ex02-25agosto2025.py
@@ -0,0 +1,19 @@
+"""
+EXERCÍCIOS: MÉTODO PARAMÉTRICO
+
+Determinar as coordenadas planas (x,y) do ponto P, a partir da medida de três distâncias
+de pontos conhecidos A, B e C até o P.
+
+PONTO |  X (m)  |  Y(m)
+  A   | 200,00  | 400,00
+  B   | 600,00  | 700,00
+  C   | 1100,00 | 300,00
+
+Coordenadas aproximadas de 
+P = (585,00; 112,00)m
+
+Distâncias medidas com σ = 0,05m
+AP = 499,92 m
+BP = 600,02 m
+CP = 538,48 m
+"""
