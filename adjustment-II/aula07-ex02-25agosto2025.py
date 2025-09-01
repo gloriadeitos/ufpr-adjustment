8a31cff27191cb7ffc5cdf963572806010585ad5
Glória Maria <152623755+gloriadeitos@users.noreply.github.com>
2025-09-01 14:15:40 -0300

    Update aula07-ex02-25agosto2025.py

--- adjustment-II/aula07-ex02-25agosto2025.py
+++ adjustment-II/aula07-ex02-25agosto2025.py
@@ -17,3 +17,25 @@ AP = 499,92 m
 BP = 600,02 m
 CP = 538,48 m
 """
+
+import numpy as np
+
+# Matriz das observações (Lb)
+Lb = np.array([
+              [200, 400],
+              [600, 700],
+              [1100, 300]
+])
+
+# Matriz dos pesos (P)
+Lb = np.array([
+              []
+])
+
+
+# Matriz dos parâmetros ajustados (Xo)
+Xo = np.array([
+              [449.92],
+              [600.02],
+              [538.48]
+])
